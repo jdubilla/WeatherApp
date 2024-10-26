ca5b1d99c6d55d4a0ef2c91d0fe15d263d55df3e
jb <jbdubillard@hotmail.com>
2024-10-26 14:31:56 +0200

    Initialize Xcode project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jean-baptiste DUBILLARD on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
